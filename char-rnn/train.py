a5b80914042dedcc7fd9aa250729db1540d6486d
Natasha Jaques <jaquesn@mit.edu>
2017-01-28 19:43:08 -0500

    change default filename ha

--- char-rnn/train.py
+++ char-rnn/train.py
@@ -12,7 +12,7 @@ from model import Model
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default='data/tinyshakespeare',
+    parser.add_argument('--data_dir', type=str, default='/home/natasha/Dropbox (MIT)/SideProjects/HangoutsCharRNN/rnn_input/',
                        help='data directory containing input.txt')
     parser.add_argument('--save_dir', type=str, default='save',
                        help='directory to store checkpointed models')
